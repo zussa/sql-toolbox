f94094cc379d882265786aa215565be78b444d3a
Humberto Rodrigo Zussa <humberto.zussa@gmail.com>
2023-01-18 09:21:47 -0300

    Script lista de servidores com cores para licenciamento

---
+++ Powershell/Licenciamento/Cores_to_CPU_Ratio.ps1
@@ -0,0 +1,76 @@
+﻿param([string]$SQLServerList=$(Throw `
+"Paramater missing: -SQLServerList ConfigGroup"))
+
+
+Function Get-CPUInfo{
+    [CmdletBinding()]
+    Param(
+    [parameter(Mandatory = $TRUE,ValueFromPipeline = $TRUE)]   [String] $ServerName
+
+    )
+
+    Process{
+    
+            # Get Default SQL Server instance's Edition
+            $sqlconn = new-object System.Data.SqlClient.SqlConnection(`
+                        "server=$ServerName;Trusted_Connection=true");
+            $query = "SELECT SERVERPROPERTY('Edition') AS Edition, SERVERPROPERTY('NumLicenses') AS NumLicenses, SERVERPROPERTY('IsClustered') AS IsClustered, SERVERPROPERTY('ComputerNamePhysicalNetBIOS') AS ComputerNamePhysical, SERVERPROPERTY('MachineName') AS MachineName;"
+
+            $sqlconn.Open()
+            $sqlcmd = new-object System.Data.SqlClient.SqlCommand ($query, $sqlconn);
+            $sqlcmd.CommandTimeout = 0;
+            $dr = $sqlcmd.ExecuteReader();
+
+            while ($dr.Read()) { 
+             $SQLEdition = $dr.GetValue(0); 
+             $NumLicenses = $dr.GetValue(1);
+             $IsClustered = $dr.GetValue(2);
+             $ComputerName = $dr.GetValue(3);
+             $MachineName = $dr.GetValue(4);
+
+             }
+
+            $dr.Close()
+            $sqlconn.Close()
+
+   
+            #Get processors information            
+            $CPU=Get-WmiObject -ComputerName $MachineName -class Win32_Processor
+            #Get Computer model information
+            $OS_Info=Get-WmiObject -ComputerName $MachineName -class Win32_ComputerSystem
+            
+     
+           #Reset number of cores and use count for the CPUs counting
+           $CPUs = 0
+           $Cores = 0
+           
+           foreach($Processor in $CPU){
+
+           $CPUs = $CPUs+1   
+           
+           #count the total number of cores         
+           $Cores = $Cores+$Processor.NumberOfCores
+        
+          } 
+           
+           $InfoRecord = New-Object -TypeName PSObject -Property @{
+                    Server = $ServerName;
+                    Model = $OS_Info.Model;
+                    CPUNumber = $CPUs;
+                    MachineName = $MachineName;
+                    ComputerName = $ComputerName;
+                    IsClustered = $IsClustered;
+                    NumLicenses = $NumLicenses;
+                    TotalCores = $Cores;
+                    Edition = $SQLEdition;
+                    'Cores to CPUs Ratio' = $Cores/$CPUs;
+                    Resume = if ($SQLEdition -like "Developer*") {"N/A"} `
+                        elseif ($Cores -eq $CPUs) {"No licensing changes"} `
+                        else {"licensing costs increase in " + $Cores/$CPUs +" times"};
+    }
+   Write-Output $InfoRecord
+          }
+}
+
+#loop through the server list and get information about CPUs, Cores and Default instance edition
+Get-Content $SQLServerList | Foreach-Object {Get-CPUInfo $_ }|Format-Table -AutoSize Server, MachineName, ComputerName, Model, Edition, IsClustered, CPUNumber, TotalCores, 'Cores to CPUs Ratio', NumLicenses
---
+++ Powershell/Licenciamento/README.md
@@ -0,0 +1,10 @@
+# Lista de servidores para licenciamento
+
+Copie o arquivo `Cores_to_CPU_Ratio.ps1` para uma pasta dentro do servidor, atualize a lista de servidores `servidores-prod.txt`  e execute o seguinte comando no *Powershell*
+
+```powershell
+.\Cores_to_CPU_Ratio.ps1 -SQLServerList "B:\Savati\servidores-prod.txt" 
+```
+
+
+
